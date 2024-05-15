9fbf5ff17ff82e11ee0469a01aa1139855e93e87
ryota-wabuchi <ryota-wabuchi@cybozu.co.jp>
2024-05-15 09:19:00 +0900

    feat: add a validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { TodoModule } from './todo.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(TodoModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
