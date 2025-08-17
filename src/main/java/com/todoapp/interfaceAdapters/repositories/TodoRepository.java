d3581902354b49154bd60c88f5a1d352d55ba2c4
ryota-wabuchi <ryota-wabuchi@cybozu.co.jp>
2025-08-18 00:37:38 +0900

    fix: fix pointed out

--- src/main/java/com/todoapp/interfaceAdapters/repositories/TodoRepository.java
+++ src/main/java/com/todoapp/interfaceAdapters/repositories/TodoRepository.java
@@ -5,12 +5,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
-    @Query("SELECT t FROM Todo t WHERE t.id = :id")
-    Optional<Todo> findById(long id);
-
     @Query("SELECT t FROM Todo t ORDER BY t.status, t.createdAt DESC")
     List<Todo> findTodoList();
 } 
